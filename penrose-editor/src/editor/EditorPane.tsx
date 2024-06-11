445c52311545d03676d63b32bb79e828df56bed9
Kyle Lee <kylelee.sea@gmail.com>
2024-06-11 16:20:40 -0400

    working keyword context sensitive autocomplete

--- penrose-editor/src/editor/EditorPane.tsx
+++ penrose-editor/src/editor/EditorPane.tsx
@@ -2,12 +2,12 @@ import CodeMirror from '@uiw/react-codemirror';
 import React from 'react'
 import {domainLanguageSupport} from './parser/domain/domainLanguage'
 import {autocompletion} from '@codemirror/autocomplete';
-import UseAutocomplete from './hooks/useAutocomplete'
+import DomainAutocomplete from './hooks/domain/domainAutocomplete'
 import { color } from '@uiw/codemirror-extensions-color';
 import {useLint} from './hooks/useLinter'
 
 export default function EditorPane() {
-  const completionFn = UseAutocomplete()
+  const completionFn = DomainAutocomplete()
   const extensionsList = [autocompletion({override: [completionFn]}),  
   domainLanguageSupport(), color]
 
