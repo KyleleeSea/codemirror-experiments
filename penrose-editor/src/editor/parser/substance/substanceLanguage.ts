2cc84b038b8c4c3ef938763369254274cf005fef
Kyle Lee <kylelee.sea@gmail.com>
2024-06-12 17:20:33 -0400

    keyword highlighting

--- penrose-editor/src/editor/parser/substance/substanceLanguage.ts
+++ penrose-editor/src/editor/parser/substance/substanceLanguage.ts
@@ -6,7 +6,18 @@ export const substanceLanguage = LRLanguage.define({
   parser: parser.configure({    
     props: [
       styleTags({
-
+        LineComment: t.lineComment,
+        BlockComment: t.lineComment,
+        for: t.keyword,
+        in: t.keyword,
+        where: t.keyword,
+        Let: t.keyword,
+        All: t.keyword,
+        AutoLabel: t.keyword,
+        Label: t.keyword,
+        NoLabel: t.keyword,
+        TeX: t.string,
+        String: t.string
       }),
     ],
   }),
